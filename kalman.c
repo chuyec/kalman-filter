17173321bf36b5d8b0d83d193e0bceed05fdb941
Denis Shreiber <chuyecd@gmail.com>
2022-11-29 15:32:38 +0700

    Add u16 data type filtering

--- kalman.c
+++ kalman.c
@@ -9,6 +9,8 @@
  * 
  */
 
+#include <math.h>
+
 #include "kalman.h"
 
 int kalman_init_f(kalman_filter_t *filter, float k, float val) {
@@ -25,16 +27,44 @@ int kalman_init_f(kalman_filter_t *filter, float k, float val) {
     return 0;
 }
 
+int kalman_init_u16(kalman_filter_t *filter, float k, uint16_t val) {
+    if (filter->k > 1) {
+        return -1;
+    }
+    else if (filter->k < 0) {
+        return -1;
+    }
+
+    filter->k = k;
+    filter->val_u16 = val;
+
+    return 0;
+}
+
 float kalman_f(kalman_filter_t *filter, float new_val) {
     float new_filtered_val = filter->k * filter->val_f + (1 - filter->k) * new_val;
 
     filter->val_f = new_filtered_val;
 
-    return new_filtered_val;
+    return filter->val_f;
+}
+
+uint16_t kalman_u16(kalman_filter_t *filter, uint16_t new_val) {
+    float new_filtered_val = filter->k * filter->val_u16 + (1 - filter->k) * new_val;
+
+    filter->val_u16 = (uint16_t)roundf(new_filtered_val);
+
+    return filter->val_u16;
 }
 
 void kalman_array_f(kalman_filter_t *filter, float *in, float *out, size_t length) {
     for (int q = 0; q < length; q++) {
         out[q] = kalman_f(filter, in[q]);
+    }
+}
+
+void kalman_array_u16(kalman_filter_t *filter, uint16_t *in, uint16_t *out, size_t length) {
+    for (int q = 0; q < length; q++) {
+        out[q] = kalman_u16(filter, in[q]);
     }
 }
--- kalman.h
+++ kalman.h
@@ -24,17 +24,30 @@ typedef struct {
         float val_f;        //!< Last filter value as float
         int32_t val_i32;    //!< Last filter value as i32
         uint32_t val_u32;   //!< Last filter value as u32
+        int16_t val_i16;    //!< Last filter value as i16
+        uint16_t val_u16;   //!< Last filter value as u16
+        int8_t val_i8;      //!< Last filter value as i8
+        uint8_t val_u8;     //!< Last filter value as u8
     };
 } kalman_filter_t;
 
 /**
- * @brief Filter one value using Kalman filter
+ * @brief Filter one float value using Kalman filter
  * 
  * @param filter    Filter structure
  * @param new_val   New value to filter
  * @return New filterred value
  */
 float kalman_f(kalman_filter_t *filter, float new_val);
+
+/**
+ * @brief Filter one u16 value using Kalman filter
+ * 
+ * @param filter    Filter structure
+ * @param new_val   New value to filter
+ * @return New filterred value
+ */
+uint16_t kalman_u16(kalman_filter_t *filter, uint16_t new_val);
 
 /**
  * @brief Filter array using Kalman filter
